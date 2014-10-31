d5bb7bf3e51fcfe6079c7b66eda7ab8696a44b97
Picek <picek.witkowski@gmail.com>
2014-10-31 17:20:32 +0100

    symplectic integrators werk

--- numerical1.py
+++ numerical1.py
@@ -86,27 +86,29 @@ def expl_rk4_step(f, y, t, dt, tuning=['default']):
 	yn = y+dt*(0.16666666666666666*k1+0.3333333333333333*k2+0.3333333333333333*k3+0.16666666666666666*k4)
 	return t+dt, yn
 
-
+#for suzuki and symplectic euler we need tuple of hamiltonians! grad_p T and grad_q U 
 def expl_sym_euler_step(f, y, t, dt, tuning=['default']):
-	if type(y)!=type(f(t,y)):
+	if type(f)!=tuple:
+		print "For symplectic methods we need tuple of hamiltonians grad_p T and grad_q U!"
+		return float('NaN'),float('NaN')
+	if type(y)!=type(f[0](y)) or type(y)!=type(f[1](y)):
 		print "solver error 0: type returned by RHS not matching type of data!"
 		return float('NaN'),float('NaN')
 	elif type(y)==np.ndarray:
-		if len(y)!=len(f(t,y)):
+		if len(y)!=len(f[0](y))*2 or len(y)!=len(f[1](y))*2:
 			print "solver error 1: length of array returned by RHS not matching size of data array!"
 			return float('NaN'),float('NaN')
-		if len(y)%2!=0 or len(f(t,y))%2!=0:
+		if len(y)%2!=0:
 			print "symplectic method only valid for Hamiltonian system: such system needs to be at least two-dimensional and of even dimension, which is not the case here!"
 			return float('NaN'), float('NaN')
 	else:
 		print "for symplectic method you need to have a Hamiltonian system which means f returns at least 2-dimensional vector!"
 		return float('NaN'),float('NaN')
-	#expecting that f = J^-1 grad H(p,q)!!!!!and H = T(p)+U(q)!!!!
+	#expecting that f is tuple of functions
 	halba=len(y)/2
-	pn=y[:halba]+dt*f(t,y)[:halba]
-	tmp_y=np.reshape([pn, y[halba:]])
+	pn=y[:halba]-dt*f[0](y[halba:])
 	#Ty, mosz recht, wez dwie!
-	qn=y[halba:]+dt*f(t,tmp_y)[halba:]
+	qn=y[halba:]+dt*f[1](pn)
 	yn = np.reshape([pn,qn], halba*2)
 	return t+dt, yn
 	
@@ -133,61 +135,84 @@ def expl_sym_verlet_step(f, y, t, dt, tuning=['default']):
 	return float('NaN'), float('NaN')
 	
 def expl_neri_step(f, y, t, dt, tuning=['default']):
-	if type(y)!=type(f(t,y)):
-		print "solver error 0: type returned by RHS not matching type of data!"
+	if type(f)!=tuple:
+		print "For symplectic methods we need tuple of hamiltonians grad_p T and grad_q U!"
 		return float('NaN'),float('NaN')
-	elif type(y)==np.ndarray:
-		if len(y)!=len(f(t,y)):
-			print "solver error 1: length of array returned by RHS not matching size of data array!"
-			return float('NaN'),float('NaN')
-		if len(y)%2!=0 or len(f(t,y))%2!=0:
+	#if type(y)!=type(f[0](y)) or type(y)!=type(f[1](y)):
+	#	print "solver error 0: type returned by RHS not matching type of data!"
+	#	return float('NaN'),float('NaN')
+	if type(y)==np.ndarray:
+		#if len(y)!=len(f[0](y))*2 or len(y)!=len(f[1](y))*2:
+		#	print "solver error 1: length of array returned by RHS not matching size of data array!"
+		#	return float('NaN'),float('NaN')
+		if len(y)%2!=0:
 			print "symplectic method only valid for Hamiltonian system: such system needs to be at least two-dimensional and of even dimension, which is not the case here!"
 			return float('NaN'), float('NaN')
 	else:
 		print "for symplectic method you need to have a Hamiltonian system which means f returns at least 2-dimensional vector!"
 		return float('NaN'),float('NaN')
-	#expecting that f = -J^-1 grad H(p,q)!!!!!and H = T(p)+U(q)!!!!
+	#expecting that f is tuple of functions
 	halba=len(y)/2
-	c=np.array([0.15337808457192392,-0.03986574628422823,-0.03986574628422823,0.15337808457192392,])
-	d=np.array([0.30675616914384785, -0.3864876617123043,0.30675616914384785,0.])
-	tmp_new = np.array([0.]*len(y))
+	c=np.array([0.6756035959798289,-0.17560359597982886,0.6756035959798289,-0.17560359597982886])
+	d=np.array([1.3512071919596578, -1.7024143839193153, 1.3512071919596578, 0])
+	tmp_new = np.zeros(len(y))
 	tmp_old = y.copy()
 	for i in range(len(c)):
-		tmp_new[halba:]=tmp_old[halba:]+c[i]*dt*f(tmp_old)[halba:]
-		tmp_new[:halba]=tmp_old[:halba]+d[i]*dt*f(tmp_new)[:halba]
-		tmp=tmp_new[:]
-		tmp_new=tmp_old[:]
-		tmp_old=tmp[:]
+		#print i
+		#q:
+		tmp_new[halba:]=tmp_old[halba:]+c[i]*dt*f[1](tmp_old[:halba])
+		#p:
+		tmp_new[:halba]=tmp_old[:halba]-d[i]*dt*f[0](tmp_new[halba:])
+		tmp_old, tmp_new = tmp_new, tmp_old
 	return t+dt, tmp_old
 
 def expl_suzuki_step(f, y, t, dt, tuning=['default']):
-	if type(y)!=type(f(t,y)):
-		print "solver error 0: type returned by RHS not matching type of data!"
+	if type(f)!=tuple:
+		print "For symplectic methods we need tuple of hamiltonians grad_p T and grad_q U!"
 		return float('NaN'),float('NaN')
-	elif type(y)==np.ndarray:
-		if len(y)!=len(f(t,y)):
-			print "solver error 1: length of array returned by RHS not matching size of data array!"
+	#if type(y)!=type(f[0](y)) or type(y)!=type(f[1](y)):
+	#	print "solver error 0: type returned by RHS not matching type of data!"
+	#	return float('NaN'),float('NaN')
+	if type(y)==np.ndarray:
+		#if len(y)!=len(f[0](y[halba:]))*2 or len(y)!=len(f[1](y[:halba]))*2:
+		#	print "solver error 1: length of array returned by RHS not matching size of data array!"
+		#	return float('NaN'),float('NaN')
+		if not issubclass(y.dtype.type, np.float):
+			print "Non-float value of solution may cause problems! return wth float"
 			return float('NaN'),float('NaN')
-		if len(y)%2!=0 or len(f(t,y))%2!=0:
+		if len(y)%2!=0:
 			print "symplectic method only valid for Hamiltonian system: such system needs to be at least two-dimensional and of even dimension, which is not the case here!"
 			return float('NaN'), float('NaN')
 	else:
 		print "for symplectic method you need to have a Hamiltonian system which means f returns at least 2-dimensional vector!"
 		return float('NaN'),float('NaN')
-	#expecting that f = -J^-1 grad H(p,q)!!!!!and H = T(p)+U(q)!!!!
+	#expecting that f is tuple of functions
 	halba=len(y)/2
 	c=np.array([ 0.20724476,  0.41448951, -0.12173427, -0.12173427,  0.41448951,
         0.20724476])
 	d=np.array([ 0.41448951,  0.41448951, -0.65795805,  0.41448951,  0.41448951,  0.        ])
-	tmp_new = np.array([0.]*len(y))
+#	much faster the way it is now, only require initial data to be of FLOAT 64 type
+#	tmp_new = np.zeros((len(c)+1, len(y)))
+#	tmp_new[0]=y.copy()
+#	#print len(c)
+#	for i in range(len(c)):
+#		#print i
+#		#q:
+#		tmp_new[i+1,halba:]=tmp_new[i,halba:]+c[i]*dt*f[1](tmp_new[i,:halba])
+#		#p:
+#		tmp_new[i+1,:halba]=tmp_new[i,:halba]-d[i]*dt*f[0](tmp_new[i+1,halba:])
+#		#print tmp_new[i+1]
+#		#print tmp_new[i+1]
+	tmp_new = np.zeros(len(y))
 	tmp_old = y.copy()
 	for i in range(len(c)):
-		tmp_new[halba:]=tmp_old[halba:]+c[i]*dt*f(tmp_old)[halba:]
-		tmp_new[:halba]=tmp_old[:halba]+d[i]*dt*f(tmp_new)[:halba]
-		tmp=tmp_new[:]
-		tmp_new=tmp_old[:]
-		tmp_old=tmp[:]
-	return t+dt, tmp_old	
+		#print i
+		#q:
+		tmp_new[halba:]=tmp_old[halba:]+c[i]*dt*f[1](tmp_old[:halba])
+		#p:
+		tmp_new[:halba]=tmp_old[:halba]-d[i]*dt*f[0](tmp_new[halba:])
+		tmp_old, tmp_new = tmp_new, tmp_old
+	return t+dt, tmp_old
 #any integrator
 
 # solver. first, we define dictionary of methods
@@ -230,7 +255,7 @@ def solver_nos(t0, y0, f, nos, tk, method='expl_RK4', tuning='', v=0, outform='a
 	
 	step=methods[method]
 	#we calculate number of steps
-	dt = (t0-tk)/float(nos)
+	dt = (tk-t0)/float(nos)
 		
 	if v==2: print "dt "+str(dt) 
 	
@@ -240,7 +265,7 @@ def solver_nos(t0, y0, f, nos, tk, method='expl_RK4', tuning='', v=0, outform='a
 	soln = [[t0, y0]]
 	
 	for i in range(nos-1):
-		tmp1, tmp2 = step(f, soln[i][1], soln[i][dt], 0, tuning=tuning)
+		tmp1, tmp2 = step(f, soln[i][1], soln[i][0], dt, tuning=tuning)
 		soln.append([tmp1, tmp2])
 	
 	if v>=1: print "solved. Took "+str(nos)+" steps to solve on interval ["+str(t0)+";"+str(tk)+"]"
